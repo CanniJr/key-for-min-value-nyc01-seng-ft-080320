b4ba68d8735be3680fcfc348f9c27f2026808133
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 05:01:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   smallest_value = nil 
   smallest_key = nil 
   name_hash.each do |key, value|
-    if smallest_value == nil 
-      name_hash[key][value]
-      smallest_value = name_hash[key]
+    if smallest_value == nil || value < smallest_value
+      smallest_value = value
+      smallest_key = key
     end
   end
   smallest_value
